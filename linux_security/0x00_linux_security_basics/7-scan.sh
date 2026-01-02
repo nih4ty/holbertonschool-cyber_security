d1c875744a9fb5cab682045d4df9ccd07f0b0b66
nih4ty <nihatyolchiyev01@gmail.com>
2026-01-02 08:15:20 +0000

    salam

--- linux_security/0x00_linux_security_basics/7-scan.sh
+++ linux_security/0x00_linux_security_basics/7-scan.sh
@@ -1,18 +1,2 @@
 #!/bin/bash
-
-# Root icazəsi yoxlanışı
-if [ "$EUID" -ne 0 ]; then
-    echo "Bu skripti root və ya sudo ilə işə salın"
-    exit 1
-fi
-
-# Arqument yoxlanışı
-if [ -z "$1" ]; then
-    echo "İstifadə: $0 <subnetwork və ya hostname>"
-    exit 1
-fi
-
-TARGET="$1"
-
-# Nmap scan – canlı hostların aşkarlanması
-nmap "$TARGET"
+sudo nmap "$1"
