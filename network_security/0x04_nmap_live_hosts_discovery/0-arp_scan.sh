40ed0b43f5d394bc786ec4e9964a56b68b9a9137
nih4ty <nihatyolchiyev01@gmail.com>
2026-01-11 12:36:06 +0000

    HOLBERTON

--- network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
+++ network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-# Check if subnet argument is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <subnetwork>"
-  exit 1
-fi
-
-# Run Nmap ARP scan without port scanning
 sudo nmap -PR -sn "$1"
