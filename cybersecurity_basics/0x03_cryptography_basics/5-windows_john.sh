bd1d6e96803afcc84dbc0b6891827eea68bb64d8
nih4ty <nihatyolchiyev01@gmail.com>
2026-02-06 08:34:22 -0500

    HOLBERTON

--- cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash -c $'awk \'{print "user:"$0}\' "$1" > .nt\njohn --wordlist=/usr/share/wordlists/rockyou.txt --format=nt .nt >/dev/null 2>&1\njohn --show --format=nt .nt | cut -d: -f2 > 5-password.txt' bash "$1"
+john --format=nt "$1"
