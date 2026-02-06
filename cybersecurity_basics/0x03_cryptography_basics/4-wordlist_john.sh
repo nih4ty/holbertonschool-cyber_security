69cf074870b74018c0fcde1e47ea9fd735664367
nih4ty <nihatyolchiyev01@gmail.com>
2026-02-06 08:06:21 -0500

    HOLBERTON

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash -c $'john --wordlist=<(zcat -f /usr/share/wordlists/rockyou.txt.gz) --format=raw-md5 --pot=4.pot "$1" >/dev/null 2>&1\njohn --show --format=raw-md5 --pot=4.pot "$1" | cut -d: -f2 > 4-password.txt' bash "$1"
+bash -c $'john --wordlist=<(zcat -f /usr/share/wordlists/rockyou.txt.gz) --format=raw-md5 --pot=4.pot "$1" >/dev/null 2>&1\nawk -F: \'NR==FNR{a[$1]=$2;next}{print a[$1]}\' 4.pot "$1" > 4-password.txt' bash "$1"
