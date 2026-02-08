d0255da364509d342a01b13bc3f90c10f925999c
nih4ty <nihatyolchiyev01@gmail.com>
2026-02-08 07:18:09 -0500

    HOLBERTON

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash -c $'tr -d "\r" < "$1" > /tmp/hashes_clean.txt\njohn --wordlist=/usr/share/wordlists/rockyou.txt --format=Raw-SHA256 /tmp/hashes_clean.txt\njohn --show --format=Raw-SHA256 /tmp/hashes_clean.txt | awk -F: '\''NF>1{print $2}'\'' > 4-password.txt' _ "$1"
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --show "$1" | awk -F: '{print $2}' > 4-password.txt
