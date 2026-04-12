67e8066d219e1f45898d9575af17345e9dccd3fa
nih4ty <nihatyolchiyev01@gmail.com>
2026-04-12 08:14:02 -0400

    HOLBERTON

--- web_application_security/0x0b_web_application_fast_incident_response/2-count_attack.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/2-count_attack.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-cat $1 | awk '{print $1}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $1}'
+
+log_file=${1:-logs.txt}
+
+# Find attacker IP
+attacker_ip=$(awk '{print $1}' "$log_file" | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}')
+
+# Count total requests by attacker
+awk -v ip="$attacker_ip" '$1==ip {count++} END {print count}' "$log_file"
